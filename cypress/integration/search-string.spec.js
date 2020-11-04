80641aefd45c3294015fdeff826502ea2a289ac7
saifsms91 <saifsms91@rediffmail.com>
2020-11-04 15:03:36 +0800

    Updated Test Suite Description

--- cypress/integration/search-string.spec.js
+++ cypress/integration/search-string.spec.js
@@ -1,7 +1,7 @@
 import {BasePage} from '../page-objects/base-page';
 import {HomePage} from '../page-objects/home-page';
 
-describe('Way 2 automation', () => {
+describe('Search string and Print in Log', () => {
   const basePage = new BasePage();
   const homePage = new HomePage();
 
@@ -10,7 +10,6 @@ describe('Way 2 automation', () => {
   });
 
   it('Search the page with string “test” and print out the search result in log', () => {
-    // homePage.serachTest()
     const log = Cypress.log(homePage.serachTest());
     const l = Cypress.log(cy.get('body'));
     const t = Cypress.log(cy.get('[ng-controller=mainCtrl]'));
